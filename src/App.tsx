28f95141de20a5571b688585f4214fd18a9094cc
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-02-02 12:55:23 +0100

    Added cusomized Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {Accordion} from './components/Accordion'
 import './App.css'
 
@@ -7,25 +8,41 @@ type AccordionData = {
   content: string;
 }
 
-const data:AccordionData = [
+const data: AccordionData[] = [
   {id: "1", title: "What's your name?", content: "My name is John Doe" },
   {id: "2", title: "What do you do?", content: "I am a Software Engineer"},
   {id: "3", title: "What do you like in free time?", content: "I like to play guitar"},
   {id: "4", title: "What's the Lun Dev channel about?", content: "It is about programming and software development"}
 ]
 
+
+
 function App() {
-  const accordionItems = data.map((item) => (
+  const [favFood, setFavFood] = useState<string>("pizza");
+
+  const onRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => setFavFood(e.target.id);
+
+  const accordionItems = data.map((item:AccordionData) => (
     <Accordion key={item.id} id={item.id} title={item.title}>
       <p>{item.content}</p>
     </Accordion>
     )
   );
 
+  console.log(favFood);
+
   return (
     <>
       <section className="accordionList">
         {accordionItems}
+        <Accordion key="100" id="100" title="Select your favorite food:">
+          <form>
+            <label htmlFor="pizza"><input id="pizza" type="radio" name="food" onChange={onRadioChange} checked={favFood==="pizza"}></input> Pizza</label>
+            <label htmlFor="hamburger"><input id="hamburger" type="radio" name="food" onChange={onRadioChange} checked={favFood==="hamburger"}></input> Hamburger</label>
+            <label htmlFor="pasta"><input id="pasta" type="radio" name="food" onChange={onRadioChange} checked={favFood==="pasta"}></input> Pasta</label>
+            <label htmlFor="sushi"><input id="sushi" type="radio" name="food" onChange={onRadioChange} checked={favFood==="sushi"}></input> Sushi</label>
+          </form>
+        </Accordion>
       </section>
     </>
   )
